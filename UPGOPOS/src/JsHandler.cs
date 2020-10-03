ac3cbde8b9dea456e23f141206ae0d6817cf9b95
Hung Nguyen <hungnv.hanu@gmail.com>
2020-10-03 12:20:43 +0700

    initialize

---
+++ UPGOPOS/src/JsHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPGOPOS.src
+{
+    class JsHandler
+    {
+
+        public void test(dynamic data)
+        {
+            Console.WriteLine(">>>>>>>> ");
+            Console.WriteLine(data["id"]);
+            Console.WriteLine(data["salesorder_no"]);
+            MessageBox.Show("TESTED");
+        }
+    }
+}
---
+++ UPGOPOS/src/Main.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CefSharp;
+using CefSharp.WinForms;
+
+using UPGOPOS.src;
+
+namespace UPGOPOS
+{
+    public partial class Main : Form
+    {
+        public ChromiumWebBrowser browser;
+        public Main()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Name = "Main";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.ResumeLayout(false);
+        }
+
+        public void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void InitBrowser()
+        {
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            browser = new ChromiumWebBrowser("http://localhost:8888");
+            // add browser to form
+            this.Controls.Add(browser);
+            // Make the browser fill the form
+            browser.Dock = DockStyle.Fill;
+
+            browser.LoadingStateChanged += OnLoadingStateChanged;
+
+            browser.RegisterJsObject("jsHandler", new JsHandler());
+
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
+        {
+            if (!args.IsLoading)
+            {
+                // Page has finished loading, do whatever you want here
+                // browser.ShowDevTools();
+            }
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
